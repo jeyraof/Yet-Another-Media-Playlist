fcd7119a3ad72c5001be8575f09d5c8530edb2a1
Jaeyoung Lee <jaeyoung@monodiary.net>
2014-05-17 11:42:33 +0900

    declare model base

--- yayp/models/__init__.py
+++ yayp/models/__init__.py
@@ -1,2 +1,23 @@
 # -*- coding: utf-8 -*-
-__author__ = 'Jaeyoung'
+
+from sqlalchemy.ext.declarative import declarative_base, declared_attr
+
+
+class DefaultBase(object):
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__
+
+    __table_args__ = {
+        'mysql_charset': 'utf8',
+        'mysql_engine': 'InnoDB',
+    }
+
+    def __repr__(self):
+        return '<%s id=%d>' % (type(self).__name__, self.id)
+
+Base = declarative_base(cls=DefaultBase)
+
+__all__ = [
+    'user'
+]
