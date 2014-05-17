164fca86a098da0836911c8df3b00f517ca946b5
Jaeyoung Lee <jaeyoung@monodiary.net>
2014-05-18 01:35:09 +0900

    add playlist and association between media and this.

--- yamp/models/media.py
+++ yamp/models/media.py
@@ -1,11 +1,30 @@
 # -*- coding: utf-8 -*-
 
-from sqlalchemy import Column, types
+from sqlalchemy import Column, types, ForeignKey, Table
+from sqlalchemy.orm import relationship
 from yamp.models import Base, IntegerChoiceType, JSONEncodedDict
 
 
+playlist_media_association = Table(u'playlist_media_assoc', Base.metadata,
+    Column('playlist_id', types.Integer, ForeignKey(u'playlist.id')),
+    Column('media_id', types.Integer, ForeignKey(u'media.id_int'))
+)
+
+
+class Playlist(Base):
+    __tablename__ = u'playlist'
+
+    id = Column('id', types.Integer, nullable=False, primary_key=True, autoincrement=True)
+    title = Column('title', types.Unicode(255), nullable=False, default=u'')
+    owner = Column('owner_id', types.Integer, ForeignKey(u'user.id_int'))
+
+    medias = relationship(u'Media',
+                          secondary=playlist_media_association,
+                          backref=u'playlist_list')
+
+
 class Media(Base):
-    __tablename__ = u'yamp_media'
+    __tablename__ = u'media'
 
     id_int = Column('id_int', types.Integer, nullable=False, primary_key=True, autoincrement=True)
     id_str = Column('id_str', types.Unicode(255), nullable=False, default=u'', index=True)
