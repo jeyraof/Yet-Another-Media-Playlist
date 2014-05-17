2f6285f2259202e6c35dd3da346544090c41a2b1
Jaeyoung Lee <jaeyoung@monodiary.net>
2014-05-17 10:42:25 +0900

    register views to app

--- yayp/__init__.py
+++ yayp/__init__.py
@@ -1,2 +1,8 @@
 # -*- coding: utf-8 -*-
-__author__ = 'Jaeyoung'
+from importlib import import_module
+from yayp import views
+from yayp.app import app
+
+
+for view_name in views.__all__:
+    import_module('.views.' + view_name, package=__name__).apply_view(app)
