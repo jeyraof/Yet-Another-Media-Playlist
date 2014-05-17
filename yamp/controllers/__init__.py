cb08a3215d01a33d689ddf2530d6db5558209898
Jaeyoung Lee <jaeyoung@monodiary.net>
2014-05-17 15:02:26 +0900

    add base controller class

--- yamp/controllers/__init__.py
+++ yamp/controllers/__init__.py
@@ -1,2 +1,11 @@
 # -*- coding: utf-8 -*-
-__author__ = 'Jaeyoung'
+
+
+class BaseController(object):
+    def __init__(self, obj=None):
+        self.obj = obj
+
+
+def register_controller(model_cls, controller_cls):
+    model_cls.controller = controller_cls
+    controller_cls.model = model_cls
