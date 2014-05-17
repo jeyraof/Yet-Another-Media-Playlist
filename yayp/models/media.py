1b5277b106e9e32e1521447b32af179c94d7d348
Jaeyoung Lee <jaeyoung@monodiary.net>
2014-05-17 12:18:15 +0900

    add media model

---
+++ yayp/models/media.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from sqlalchemy import Column, types
+from yayp.models import Base, IntegerChoiceType, JSONEncodedDict
+
+
+class Media(Base):
+    __tablename__ = 'yayp_media'
+
+    id_int = Column('id_int', types.Integer, nullable=False, primary_key=True, autoincrement=True)
+    id_str = Column('id_str', types.Unicode(255), nullable=False, default=u'', index=True)
+    media_type = Column('media_type',
+                        IntegerChoiceType(choices=[
+                            (1, 'youtube'),
+                        ]),
+                        nullable=False, default=0, index=True)
+    data = Column('data', JSONEncodedDict, nullable=True, default=u'')
