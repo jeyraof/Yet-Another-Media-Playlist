3c3c8647ca2ba598f184fd49f076b57f92ee19fe
Jaeyoung Lee <jaeyoung@monodiary.net>
2014-05-18 01:34:01 +0900

    send variable to jinja2 for testing

--- yamp/views/main.py
+++ yamp/views/main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
 
 view = Blueprint('main', __name__)
 
@@ -11,4 +11,4 @@ def apply_view(app):
 
 @view.route('/')
 def index():
-    return render_template('main/index.html')
+    return render_template('main/index.html', user_id=session.get('id_int'))
