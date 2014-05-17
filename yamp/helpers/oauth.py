2a7addfba28a5ab67d4c35477ac105619df8b1a4
Jaeyoung Lee <jaeyoung@monodiary.net>
2014-05-17 15:02:48 +0900

    write down google api object and google token getter

--- yamp/helpers/oauth.py
+++ yamp/helpers/oauth.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from flask import session
 from flask_oauth import OAuth
 from yamp.app import app
 
@@ -7,7 +8,7 @@ config = app.config
 oauth = OAuth()
 
 google = oauth.remote_app('google',
-                          base_url='https://www.google.com/accounts/',
+                          base_url='https://www.googleapis.com/',
                           authorize_url='https://accounts.google.com/o/oauth2/auth',
                           request_token_url=None,
                           request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email',
@@ -17,3 +18,21 @@ google = oauth.remote_app('google',
                           access_token_params={'grant_type': 'authorization_code'},
                           consumer_key=config.get('GOOGLE_CLIENT_ID'),
                           consumer_secret=config.get('GOOGLE_CLIENT_SECRET'))
+GOOGLE_TOKEN_NAME = 'google_access_token'
+
+@google.tokengetter
+def get_google_access_token():
+    return session.get(GOOGLE_TOKEN_NAME)
+
+
+class GoogleAPI(object):
+    def __init__(self, access_token):
+        self.access_token = access_token
+
+    def fetch(self):
+        param = {
+            'access_token': self.access_token,
+            'alt': 'json',
+        }
+        r = google.get('/oauth2/v2/userinfo', data=param)
+        return r
