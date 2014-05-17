2f5777a5477b24031f59a461098fba49c9207e9a
Jaeyoung Lee <jaeyoung@monodiary.net>
2014-05-17 12:18:04 +0900

    declare custom playlist

--- yayp/models/__init__.py
+++ yayp/models/__init__.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from sqlalchemy.ext.declarative import declarative_base, declared_attr
+from sqlalchemy.types import TypeDecorator, Integer, UnicodeText
+import json
+
+__all__ = [
+    'user',
+    'media',
+]
 
 
 class DefaultBase(object):
@@ -18,6 +25,30 @@ class DefaultBase(object):
 
 Base = declarative_base(cls=DefaultBase)
 
-__all__ = [
-    'user'
-]
+
+class IntegerChoiceType(TypeDecorator):
+    impl = Integer
+
+    def __init__(self, choices, **kw):
+        self.choices = dict(choices)
+        super(IntegerChoiceType, self).__init__(**kw)
+
+    def process_bind_param(self, value, dialect):
+        return [k for k, v in self.choices.iteritems() if v == value][0]
+
+    def process_result_value(self, value, dialect):
+        return self.choices[value]
+
+
+class JSONEncodedDict(TypeDecorator):
+    impl = UnicodeText
+
+    def process_bind_param(self, value, dialect):
+        if value is not None:
+            value = json.dumps(value)
+        return value
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            value = json.loads(value)
+        return value
