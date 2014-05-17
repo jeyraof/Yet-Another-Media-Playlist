4651a51a197b16c77e3ed88de6ba0fa3a323847c
Jaeyoung Lee <jaeyoung@monodiary.net>
2014-05-17 11:42:42 +0900

    add user model

---
+++ yayp/models/user.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from sqlalchemy.sql.expression import func
+from sqlalchemy import Column, types, Index
+from yayp.models import Base
+
+
+class User(Base):
+    __tablename__ = u'yayp_user'
+
+    id_int = Column('id_int', types.Integer, nullable=False, primary_key=True, autoincrement=True)
+    id_str = Column('id_str', types.Unicode(255), nullable=False, default=u'', index=True)
+    email = Column('email', types.Unicode(255), nullable=False, default=u'', index=True)
+    password = Column('password', types.Unicode(255), nullable=False, default=u'')
+
+    registered_at = Column('mb_datetime', types.DateTime, nullable=False, default=func.now(), index=True)
