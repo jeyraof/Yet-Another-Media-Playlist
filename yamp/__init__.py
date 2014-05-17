6bb751b12a43780873904627917aa621f85a3f12
Jaeyoung Lee <jaeyoung@monodiary.net>
2014-05-18 01:34:44 +0900

    configure session before request such as user object

--- yamp/__init__.py
+++ yamp/__init__.py
@@ -1,8 +1,22 @@
 # -*- coding: utf-8 -*-
+from flask import session, g
 from importlib import import_module
 from yamp import views
-from yamp.app import app
+from yamp.app import app, db
+from yamp.models.user import User
 
 
 for view_name in views.__all__:
     import_module('.views.' + view_name, package=__name__).apply_view(app)
+
+
+@app.before_request
+def before_request():
+    session.permanent = True
+    g.user = None
+
+    def set_user_by_id_int():
+        g.user = db.query(User).get(session['id_int'])
+
+    if 'id_int' in session:
+        set_user_by_id_int()
