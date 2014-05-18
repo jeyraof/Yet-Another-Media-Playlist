a150b20f3636cc50e6724d1cac4d38cd0c17c8f3
Jaeyoung Lee <jaeyoung@monodiary.net>
2014-05-18 16:42:49 +0900

    add login_required decorator

---
+++ yamp/helpers/user.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from functools import wraps
+from flask import g, redirect, url_for, flash
+
+
+def login_required(f):
+    @wraps(f)
+    def return_func(*args, **kwargs):
+        if g.user:
+            return f(*args, **kwargs)
+
+        flash(u'로그인 후 이용하실 수 있습니다.')
+        return redirect(url_for('main.index'))
+    return return_func
