a1d4fa532eced7bb148c3b6e157628fdb76f44ad
Jaeyoung Lee <jaeyoung@monodiary.net>
2014-05-18 19:02:19 +0900

    add default info to context_processor write down int_with_comma filter

--- yamp/__init__.py
+++ yamp/__init__.py
@@ -4,12 +4,16 @@ from importlib import import_module
 from yamp import views
 from yamp.app import app, db
 from yamp.models.user import User
+from yamp.controllers.user import UserController
+from yamp.controllers.playlist import PlaylistController
 
 
+# Apply views to app
 for view_name in views.__all__:
     import_module('.views.' + view_name, package=__name__).apply_view(app)
 
 
+# Process of before request
 @app.before_request
 def before_request():
     session.permanent = True
@@ -19,8 +23,17 @@ def before_request():
         g.user = db.query(User).get(session['id_int'])
 
 
+# Context processor
 @app.context_processor
 def context_processor():
     return {
         'user': g.user,
+        'archived_playlist': UserController.get_archived_playlist(),
+        'playlist_number': PlaylistController.get_playlist_number(),
     }
+
+
+# Custom filters
+@app.template_filter()
+def int_with_comma(n):
+    return format(n, ',d')
