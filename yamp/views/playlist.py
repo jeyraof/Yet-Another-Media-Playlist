a779275f8b09769096407690c17cc5bb9c9bcea0
Jaeyoung Lee <jaeyoung@monodiary.net>
2014-05-31 02:28:26 +0900

    add playlist views to use for media_list

--- yamp/views/playlist.py
+++ yamp/views/playlist.py
@@ -9,3 +9,16 @@ view = Blueprint('playlist', __name__, url_prefix='/playlist')
 
 def apply_view(app):
     app.register_blueprint(view)
+
+
+@view.route('/<id_int>')
+def media_list(id_int):
+    playlist = PlaylistController.get_playlist_by_id_int(id_int)
+    all_media = playlist.media_list
+
+    opt = {
+        'playlist': playlist,
+        'media_list': all_media,
+    }
+
+    return render_template('playlist/medialist.html', **opt)
