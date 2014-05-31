5760daf998e82d12942197495bba5a2b2f687fd1
Jaeyoung Lee <jaeyoung@monodiary.net>
2014-05-31 22:24:47 +0900

    add api to playlist

--- yamp/views/playlist.py
+++ yamp/views/playlist.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from flask import Blueprint, url_for, session, flash, redirect, render_template, request
+from flask import Blueprint, render_template, request, jsonify
 from yamp.controllers.playlist import PlaylistController
-from yamp.helpers.user import login_required
 
 view = Blueprint('playlist', __name__, url_prefix='/playlist')
 
@@ -16,9 +15,23 @@ def media_list(id_int):
     playlist = PlaylistController.get_playlist_by_id_int(id_int)
     all_media = playlist.media_list
 
-    opt = {
-        'playlist': playlist,
-        'media_list': all_media,
-    }
+    res_type = request.args.get('v', None)
+    if res_type == 'json':
+        tmp_list = []
+        for media in all_media:
+            tmp_list.append({
+                'media_type': media.media_type,
+                'title': media.title,
+                'duration': media.duration,
+                'url': media.url,
+            })
 
-    return render_template('playlist/medialist.html', **opt)
+        return jsonify(media_list=tmp_list)
+
+    else:
+        opt = {
+            'playlist': playlist,
+            'media_list': all_media,
+        }
+
+        return render_template('playlist/medialist.html', **opt)
